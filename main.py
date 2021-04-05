9ea75f42c0049f8401b22938d30a8ff22279df89
nunnito <nunnito@tutanota.com>
2021-04-05 10:38:59 -0600

    Move content from nunnix_manga.py to main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-if __name__ == "__main__":
-    import nunnix_manga.nunnix_manga
+from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtGui import QGuiApplication
+from pathlib import Path
+from os import environ
+from nunnix_manga.utils import settings
+import sys
+
+# Set environment variables
+environ["QT_QUICK_CONTROLS_STYLE"] = "Material"
+environ["QT_QUICK_CONTROLS_MATERIAL_VARIANT"] = "Dense"
+
+# Set init variables
+application = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
+
+# Load QML file and execute it
+engine.load(str(Path(__file__).parent / "nunnix_manga" / "qml" / "main.qml"))
+sys.exit(application.exec_())
