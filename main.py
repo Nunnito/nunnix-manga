b4759356210eccf15391eb1c3fbe923f5ad744ac
nunnito <nunnito@tutanota.com>
2021-04-09 07:30:26 -0600

    Fix imports

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtGui import QGuiApplication
 from pathlib import Path
 from os import environ
-from nunnix_manga.utils import settings
+from app.utils import settings
 import sys
 
 # Set environment variables
@@ -18,5 +18,5 @@ context = engine.rootContext()
 context.setContextProperty("thm", settings.get_theme_file_content())  # Theme
 
 # Load QML file and execute it
-engine.load(str(Path(__file__).parent / "nunnix_manga" / "qml" / "main.qml"))
+engine.load(str(Path(__file__).parent / "app" / "ui" / "main.qml"))
 sys.exit(application.exec_())
