d04ca4beced3945511262179f9cbb2af07707efb
nunnito <nunnito@tutanota.com>
2021-05-16 01:47:06 -0600

    Add main function

--- app/main.py
+++ app/main.py
@@ -7,30 +7,35 @@ from pathlib import Path
 from os import environ
 import sys
 
+# Set init variables
+application = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
+context = engine.rootContext()
+
 
 def before_close():
     """Actions before the application exits"""
     del globals()["engine"]
 
 
-# Set environment variables
-environ["QT_QUICK_CONTROLS_STYLE"] = "Material"
-environ["QT_QUICK_CONTROLS_MATERIAL_VARIANT"] = "Dense"
+def main():
+    # Set environment variables
+    environ["QT_QUICK_CONTROLS_STYLE"] = "Material"
+    environ["QT_QUICK_CONTROLS_MATERIAL_VARIANT"] = "Dense"
 
-# Set init variables
-application = QGuiApplication(sys.argv)
-engine = QQmlApplicationEngine()
-context = engine.rootContext()
+    # Load variables to QML
+    icon_engine = Icon()
+    context.setContextProperty("thm", settings.get_theme_file_content())  # Theme
+    context.setContextProperty("Icon", icon_engine)
 
-# Load variables to QML
-icon_engine = Icon()
-context.setContextProperty("thm", settings.get_theme_file_content())  # Theme
-context.setContextProperty("Icon", icon_engine)
+    # Set application properties
+    application.aboutToQuit.connect(before_close)
+    application.setWindowIcon(QIcon(icon_engine.get_icon("app.svg")))
 
-# Set application properties
-application.aboutToQuit.connect(before_close)
-application.setWindowIcon(QIcon(icon_engine.get_icon("app.svg")))
+    # Load QML file and execute it
+    engine.load(str(Path(__file__).parent / "ui" / "main.qml"))
+    sys.exit(application.exec_())
 
-# Load QML file and execute it
-engine.load(str(Path(__file__).parent / "ui" / "main.qml"))
-sys.exit(application.exec_())
+
+if __name__ == "__main__":
+    main()
