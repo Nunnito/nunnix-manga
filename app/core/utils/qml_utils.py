1914faf8d6cb47fc556d4a56d1dba3b1579137e9
Nunnito Nevermind <nunnito@pm.me>
2022-04-16 05:40:02 -0600

    Add get_theme and  qobject_to_dict

--- app/core/utils/qml_utils.py
+++ app/core/utils/qml_utils.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtCore import QObject, QVariant, pyqtSlot
+
+from core.utils import settings
 
 
 class Icon(QObject):
@@ -16,3 +18,39 @@ class Icon(QObject):
             str: Icon absolute path (even if it doesn't exist)
         """
         return str(Path(__file__).parents[2] / "resources" / "icons" / icon)
+
+
+class Theme(QObject):
+    """This class will be used in QML to get the current theme"""
+
+    @pyqtSlot(QObject, QObject, result=QVariant)
+    def get_theme(self, dark: QObject, light: QObject) -> QVariant:
+        """Get current theme
+
+        Returns:
+            str: Current theme
+        """
+        theme = settings.get_theme_file_content()
+        dark_theme = qobject_to_dict(dark)
+        dark_theme.update(theme)
+        theme = dark_theme
+
+        return theme
+
+
+def qobject_to_dict(qobject: QObject) -> dict:
+    """Convert a QObject to a dict
+
+    Args:
+        qbject (QObject): QObject to convert
+
+    Returns:
+        dict: Converted QObject
+    """
+    result = {}
+    meta_object = qobject.metaObject()
+
+    for i in range(meta_object.propertyOffset(), meta_object.propertyCount()):
+        property = meta_object.property(i)
+        result[property.name()] = qobject.property(property.name())
+    return result
