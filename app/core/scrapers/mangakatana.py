a511f86f605d0551c94b069c37ce57cc67153d67
Nunnito Nevermind <nunnito@pm.me>
2022-04-21 01:51:06 -0600

    Add get_chapters_images and search_manga functions

--- app/core/scrapers/mangakatana.py
+++ app/core/scrapers/mangakatana.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from requests import Request, Session
+
 import requests
 import re
 
@@ -6,7 +8,7 @@ from bs4 import BeautifulSoup
 
 from core.utils.logger import logger
 
-
+BASE_URL = "https://mangakatana.com"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)',
     'Referer': "http://mangakatana.com/"}
@@ -35,7 +37,7 @@ def get_manga_data(url: str) -> dict:
         "title": "Kumo desu ga, nani ka?",
         "author": "Miura Kentaro",
         "description": "Some description",
-        "cover": "https://uploads.mangadex.org/covers/MUUID/CUUID.jpg.512.jpg",
+        "cover": "https://i3.mangakatana.com/token/3A32n%2Ag%3A9o%2Ab.jpg",
         "genres: ["Ecchi", "BL", "Zombies", "Yuri"],
         "status": "completed" | "ongoing" | "hiatus" | "cancelled",
         "chapters_data": {
@@ -120,4 +122,164 @@ def get_manga_data(url: str) -> dict:
     return data
 
 
-print(get_manga_data("https://mangakatana.com/manga/time-roulette.25837"))
+def get_chapters_images(url: str) -> list:
+    """ Get chapter images.
+
+    Parameters
+    ----------
+    url : str
+        The chapter URL.
+
+    Returns
+    -------
+    list
+        A list with all images links.
+
+    Example
+    -------
+        >>> get_chapter_images("https://mangakatana.com/manga/baki/c50")
+    """
+    # TODO: Status code handler
+    logger.debug(f"Requesting chapters data at {url}")
+    response = requests.get(url, headers=HEADERS)
+
+    images = re.findall(r"var ytaw=\[('.+'),\]", response.text)[0]
+    images = images.replace("'", "").split(",")
+    logger.debug("\n\nIMAGE: " + "\n\nIMAGE: ".join(images))
+
+    logger.debug("\nDone. Returning data...\n")
+    return images
+
+
+def search_manga(
+    title: str = None,
+    author: str = None,
+    order: str = "latest",
+    include_mode: str = "and",
+    status: str = None,
+    chapters: int = 1,
+    genres: list = [],
+    exclude_genres: list = [],
+    page: int = 1
+) -> dict:
+    """ Search manga, with advanced parameters.
+
+    Parameters
+    ----------
+    title : str, optional
+        Name of the title to search. To work, it must be the only parameter.
+
+    author : str, optional
+        Name of the author to search. To work, it must be the only parameter.
+
+    order : str, optional
+        Sort by different methods. Valid options:
+        "az" (alphabetically), "numc" (number of chapters), "new", "latest".
+
+    include_mode : str, optional
+        Valid options: "and" (all selected genres)
+        and "or" (any selected genre).
+
+    status : str, optional
+        Current manga status. Valid options:
+        "0" (cancelled), "1" (ongoing), "2" (completed).
+
+    chapters : string, optional
+        Minimum chapters numbers.
+
+    genres : list, optional
+        Genres name.
+
+    exclude_genres : list, optional
+        Excludes genres name.
+
+    page : int, optional
+        The page number.
+
+
+    Returns
+    -------
+    list
+        Dictionary with all manga results.
+
+    Example
+    -------
+        >>> search_manga(genres=["Ecchi", "Harem"], status="1")
+
+    List of valid genres
+    ----------------------
+    "4-koma", "action", "adult", "adventure", "artbook", "award-winning",
+    "yuri", "comedy", "cooking", "smut", "drama", "ecchi", "yaoi", "shota",
+    "fantasy", "gender-bender", "gore", "harem", "historical", "horror",
+    "isekai", "josei", "loli", "manhua", "manhwa", "martial-arts", "mecha",
+    "medical", "webtoon", "doujinshi", "one-shot", "overpowered-mc",
+    "psychological", "reincarnation", "romance", "school-life", "sci-fi",
+    "seinen", "sexual-violence", "tragedy", "shoujo", "shoujo-ai", "shounen",
+    "shounen-ai", "slice-of-life", "mystery", "sports", "super-power",
+    "supernatural", "survival", "time-travel", "music".
+    """
+    # TODO: Status code handler
+    data = []
+
+    # If title is not none, search only the title.
+    if title is not None:
+        url = f"{BASE_URL}/page/{page}?search={title}"
+    elif author is not None:
+        url = f"{BASE_URL}/page/{page}?search={author}&search_by=author"
+    else:
+        url = f"{BASE_URL}/manga/page/{page}"
+
+    # Parameters
+    payload = {
+        "filter": 1,
+        "order": order,
+        "include_mode": include_mode,
+        "status": status,
+        "chapters": chapters,
+        "genres": "_".join(genres),
+        "exclude_genres": "_".join(exclude_genres)
+    }
+
+    # Prepare requests
+    session = Session()
+    response = Request("GET", url, params=payload
+                       if title is None and author is None else None)
+    logger.debug(f"Requesting search at {response.prepare().url}")
+
+    response = session.send(response.prepare())
+    soup = BeautifulSoup(response.text, "lxml")
+
+    # If there is only a single result
+    if not soup.find(id="book_list"):
+        logger.debug("Total results: 1\n")
+
+        title = soup.find("h1", {"class": "heading"}).text.strip()
+        cover = soup.find("img", {"alt": "[Cover]"}).get("src")
+        link = soup.find("meta", {"property": "og:url"}).get("content")
+
+        data.append(
+            {
+                "title": title,
+                "link": link,
+                "cover": cover
+            }
+        )
+    else:
+        results = soup.find(id="book_list").find_all("div", {"class": "item"})
+        logger.debug(f"Total results: {len(results)}\n")
+
+        for result in results:
+            title = result.find("h3").find("a").text.strip()
+            link = result.find("h3").find("a").get("href")
+            cover = result.find("img", {"alt": "[Cover]"}).get("src")
+
+            data.append(
+                {
+                    "title": title,
+                    "link": link,
+                    "cover": cover
+                }
+            )
+
+    logger.debug("Done. Returning data...\n")
+    return data
