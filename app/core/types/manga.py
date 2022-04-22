93c56e48d6901ec1700b8c028c4ce4ea0cf5a1f8
Nunnito Nevermind <nunnito@pm.me>
2022-04-22 05:27:53 -0600

    Modify to support QML access

--- app/core/types/manga.py
+++ app/core/types/manga.py
@@ -1,52 +1,145 @@
-class Chapter:
+from PyQt5.QtCore import QObject, QVariant, pyqtSlot, pyqtProperty
+
+
+class Chapter(QObject):
     """ Chapter type to be used in Manga class """
     def __init__(self, scraper, name: str, date: list[str, str, str],
-                 link: str, scanlation: str) -> None:
-        self.scraper = scraper
-        self.name = name
-        self.date = date
-        self.link = link
-        self.scanlation = scanlation
+                 link: str, scanlation: str, parent) -> None:
+        super(Chapter, self).__init__(parent)
 
+        self._scraper = scraper
+        self._name = name
+        self._date = date
+        self._link = link
+        self._scanlation = scanlation
+
+    @pyqtProperty(str)
+    def scraper(self) -> str:
+        return self._scraper.NAME
+
+    @pyqtProperty(str)
+    def name(self) -> str:
+        return self._name
+
+    @pyqtProperty(list)
+    def date(self) -> list:
+        return self._date
+
+    @pyqtProperty(str)
+    def link(self) -> str:
+        return self._link
+
+    @pyqtProperty(str)
+    def scanlation(self) -> str:
+        return self._scanlation
+
+    @pyqtSlot(result=list)
     def get_images(self) -> list[str]:
         """ Get images from chapter """
-        return self.scraper.get_chapter_images(self.link)
+        return self._scraper.get_chapter_images(self._link)
 
 
-class ChaptersData:
+class ChaptersData(QObject):
     """ Chapters data type to be used in Manga class """
-    def __init__(self, total: int, chapters: list[Chapter]) -> None:
-        self.total = total
-        self.chapters = chapters
+    def __init__(self, total: int, chapters: list[Chapter], parent) -> None:
+        super(ChaptersData, self).__init__(parent)
+
+        self._total = total
+        self._chapters = chapters
+
+    @pyqtProperty(int)
+    def total(self) -> int:
+        return self._total
+
+    @pyqtProperty(QVariant)
+    def chapters(self) -> list[Chapter]:
+        return self._chapters
 
 
-class Manga:
+class Manga(QObject):
     """ Manga type to get all manga data """
     def __init__(self, scraper, title: str, author: str, description: str,
                  cover: str, genres: list[str], status: str, link: str,
-                 chapters_data: ChaptersData) -> None:
-        self.scraper = scraper
-        self.title = title
-        self.author = author
-        self.description = description
-        self.cover = cover
-        self.genres = genres
-        self.status = status
-        self.link = link
-        self.chapters_data = chapters_data
+                 chapters_data: ChaptersData, parent) -> None:
+        super(Manga, self).__init__(parent)
+
+        self._scraper = scraper
+        self._title = title
+        self._author = author
+        self._description = description
+        self._cover = cover
+        self._genres = genres
+        self._status = status
+        self._link = link
+        self._chapters_data = chapters_data
+
+    @pyqtProperty(str)
+    def scraper(self) -> str:
+        return self._scraper.NAME
+
+    @pyqtProperty(str)
+    def title(self) -> str:
+        return self._title
+
+    @pyqtProperty(str)
+    def author(self) -> str:
+        return self._author
+
+    @pyqtProperty(str)
+    def description(self) -> str:
+        return self._description
+
+    @pyqtProperty(str)
+    def cover(self) -> str:
+        return self._cover
+
+    @pyqtProperty(list)
+    def genres(self) -> list[str]:
+        return self._genres
+
+    @pyqtProperty(str)
+    def status(self) -> str:
+        return self._status
+
+    @pyqtProperty(str)
+    def link(self) -> str:
+        return self._link
+
+    @pyqtProperty(QVariant)
+    def chapters_data(self) -> ChaptersData:
+        return self._chapters_data
 
 
-class MangaSearch:
+class MangaSearch(QObject):
     """ Manga search type """
-    def __init__(self, scraper, title: str, link: str, cover: str) -> None:
-        self.scraper = scraper
-        self.title = title
-        self.link = link
-        self.cover = cover
+    def __init__(self, scraper, title: str, link: str, cover: str, parent):
+        super(MangaSearch, self).__init__(parent)
 
+        self._scraper = scraper
+        self._title = title
+        self._link = link
+        self._cover = cover
+
+    @pyqtProperty(str)
+    def scraper(self) -> str:
+        return self._scraper.NAME
+
+    @pyqtProperty(str)
+    def title(self) -> str:
+        return self._title
+
+    @pyqtProperty(str)
+    def link(self) -> str:
+        return self._link
+
+    @pyqtProperty(str)
+    def cover(self) -> str:
+        return self._cover
+
+    @pyqtSlot(result=QVariant)
     def get_data(self) -> Manga:
         """ Get manga data """
-        data = self.scraper.get_manga_data(self.link)
+        data = self._scraper.get_manga_data(self._link)
 
         title = data["title"]
         author = data["author"]
@@ -58,19 +151,21 @@ class MangaSearch:
         for chapter in data["chapters_data"]["chapters"]:
             chapters.append(
                 Chapter(
-                    self.scraper,
+                    self._scraper,
                     chapter["name"],
                     chapter["date"],
                     chapter["link"],
                     chapter["scanlation"],
+                    self
                 )
             )
-        chapters_data = ChaptersData(data["chapters_data"]["total"], chapters)
+        chapters_data = ChaptersData(data["chapters_data"]["total"], chapters,
+                                     self)
 
         manga = Manga(
-            self.scraper,
-            title, author, description, cover, genres, status, self.link,
-            chapters_data
+            self._scraper,
+            title, author, description, cover, genres, status, self._link,
+            chapters_data, self
         )
 
-        return manga
+        return QVariant(manga)
