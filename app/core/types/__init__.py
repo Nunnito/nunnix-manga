b3a8575ef13250833739e1d4d8801e16c8e2abdd
Nunnito Nevermind <nunnito@pm.me>
2022-04-25 04:24:31 -0600

    Rename scraper.py to explorer.py

--- app/core/types/__init__.py
+++ app/core/types/__init__.py
@@ -1,6 +1,6 @@
 from core.types.manga import Chapter, ChaptersData, Manga, MangaSearch
 from core.types.signalhandler import SignalHandler
-from core.types.scraper import Explorer
+from core.types.explorer import Explorer
 
 Chapter
 ChaptersData
