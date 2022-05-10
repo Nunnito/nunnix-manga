3287238a077ad56ccfdbb2fd97d8ba39303b865d
Nunnito <nunnito@pm.me>
2022-05-10 15:32:14 -0600

    Replace Nuitka build method with Pyinstaller

--- build.py
+++ build.py
@@ -1,25 +1,74 @@
 import sys
 from subprocess import Popen
+from shutil import make_archive
 
 
-command = [
-    sys.executable, "-m", "nuitka", "app/main.py",
-    "--show-progress",
-    "--onefile",
-    "--plugin-enable=pyqt5",
-    "--assume-yes-for-downloads",
-    "--include-data-dir=app/core=core",
-    "--include-data-dir=app/ui=ui",
-    "--include-data-dir=app/resources=resources",
-    "--show-progress",
-    "--nofollow-import-to=core",
-    "--include-package=bs4",
-    "--include-qt-plugins=sensible,qml",
-    "--remove-output"
-]
+def create_bat():
+    # Create .bat file
+    if len(sys.argv) > 1 and (sys.argv[1] == "--zip" or sys.argv[1] == "-z"):
+        with open("dist\\nunnix-manga.bat", "w") as f:
+            f.write("start Nunnix-Manga\\Nunnix-Manga.exe\n")
 
-if sys.platform == "linux":
-    command.append("--linux-onefile-icon=app/resources/icons/app.svg")
-    command.extend(["-o", "Nunnix-Manga.bin"])
+def create_zip():
+    # Create .zip file
+    make_archive("nunnix-manga", "zip", "dist")
 
-Popen(command).communicate()  # Wait for the process to finish
+def get_base_args():
+    # Command line arguments
+    command = [
+        "pyinstaller", "app/main.py",
+        "--clean",
+        "--noconfirm",
+        "--name", "Nunnix-Manga",
+        "--hidden-import", "bs4",
+        "--exclude-module", "pytest",
+        "--exclude-module", "pytest-asyncio",
+        "--icon", "NONE"
+    ]
+    return command
+
+
+def get_windows_args():
+    # Command line arguments for Windows
+    command = get_base_args()
+    command.extend([
+        "--noconsole",
+        "--add-data", "app/core;core",
+        "--add-data", "app/ui;ui",
+        "--add-data", "app/resources;resources"
+    ])
+    return command
+
+
+def get_linux_args():
+    # Command line arguments for Linux
+    command = get_base_args()
+    command.extend([
+        "--add-data", "app/core:core",
+        "--add-data", "app/ui:ui",
+        "--add-data", "app/resources:resources"
+    ])
+
+
+def build_windows():
+    # Build for Windows
+    command = get_windows_args()
+    Popen(command).communicate()  # Run command
+    create_bat()
+    create_zip()  # Create .zip file if --zip or -z is passed
+
+
+def build_linux():
+    # Build for Linux
+    command = get_linux_args()
+    Popen(command).communicate()  # Run command
+    create_zip()  # Create .zip file if --zip or -z is passed
+
+
+if __name__ == "__main__":
+    if sys.platform == "win32":
+        build_windows()
+    elif sys.platform == "linux":
+        build_linux()
+    else:
+        print("Unsupported platform")
