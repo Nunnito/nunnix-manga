8944212b92cce6d2144d54fc36ec778250ad7a4e
Nunnito Nevermind <nunnito@pm.me>
2022-05-12 09:08:25 -0600

    Add Linux cx_Freeze support

--- build.py
+++ build.py
@@ -2,6 +2,9 @@ import sys
 import os
 import shutil
 from pathlib import Path
+from urllib.request import urlretrieve
+from subprocess import Popen
+
 from cx_Freeze import setup, Executable, build
 
 
@@ -20,6 +23,12 @@ class RemoveBloatFiles(build):
     description = "Remove unnecessary files from the executable"
 
     def run(self):
+        if sys.platform == "win32":
+            self.remove_windows_bloat()
+        elif sys.platform == "linux":
+            self.remove_linux_bloat()
+
+    def remove_windows_bloat(self):
         # Remove PyQt5 dlls (very WIP, may not work)
         qt_files = ["Qt5Qml.dll", "Qt5Gui.dll", "Qt5Designer.dll",
                     "Qt5Location.dll", "Qt5Widgets.dll", "Qt5XmlPatterns.dll",
@@ -31,6 +40,86 @@ class RemoveBloatFiles(build):
             print(f"Removing {file}...")
             os.remove(file)
         os.chdir(Path(__file__).parent)  # Change dir back to the main folder
+
+    def remove_linux_bloat(self):
+        # Remove PyQt5 .so (very WIP, may not work)
+        qt_files = ["libQt5PrintSupport.so.5", "libQt5Help.so.5",
+                    "libQt5Bluetooth.so.5", "libQt5Multimedia.so.5",
+                    "libQt5Location.so.5", "libQt5XmlPatterns.so.5",
+                    "libQt5Designer.so.5", "libQt5Widgets.so.5"]
+
+        os.chdir("build/lib/PyQt5/Qt5/lib")  # Change dir to the PyQt5 folder
+        for file in qt_files:
+            print(f"Removing {file}...")
+            os.remove(file)
+        os.chdir(Path(__file__).parent)  # Change dir back to the main folder
+
+
+class MakeAppImage(build):
+    """Create an .appimage with the resources"""
+    description = "Create an .appimage with the resources"
+
+    def run(self):
+        self.download_appimagetool()
+        self.create_desktop_file()
+        self.copy_icon()
+        self.create_apprun()
+        Popen(["./appimagetool.AppImage", "build"]).communicate()
+
+    def download_appimagetool(self) -> None:
+        # Download AppImageTool
+        print("Downloading AppImageTool...")
+        urlretrieve(
+            "https://github.com/AppImage/AppImageKit/releases/download/13/" +
+            "appimagetool-x86_64.AppImage",
+            "appimagetool.AppImage")
+
+        # Make executable
+        os.chmod("appimagetool.AppImage", 0o755)
+
+    def create_desktop_file(self) -> None:
+        # Create .desktop file
+        print("Creating .desktop file...")
+        content = "[Desktop Entry]\n"
+        content += "Name=Nunnix-Manga\n"
+        content += "Exec=AppRun\n"
+        content += "Icon=icon\n"
+        content += "Type=Application\n"
+        content += "Categories=Utility;Graphics;\n"
+
+        with open("build/nunnix-manga.desktop", "w") as f:
+            f.write(content)
+
+    def copy_icon(self, name: str = "icon.svg") -> None:
+        # Copy app icon
+        print("Copying icon...")
+        shutil.copy("app/resources/icons/app.svg", f"build/{name}")
+
+    def create_apprun(self) -> None:
+        # Create AppRun file
+        print("Creating AppRun file...")
+        content = '#!/bin/bash\n'
+        content += 'HERE="$(dirname "$(readlink -f "${0}")")"\n'
+        content += 'exec "$HERE/Nunnix-Manga" "$@"\n'
+
+        with open("build/AppRun", "w") as f:
+            f.write(content)
+
+        # Make executable
+        os.chmod("build/AppRun", 0o755)
+
+
+class CleanUp(build):
+    """Clean up AppImage files"""
+    description = "Clean up AppImage files"
+
+    def run(self):
+        # Clean up AppImage files
+        print("Cleaning up...")
+        os.remove("appimagetool.AppImage")
+        os.remove("build/nunnix-manga.desktop")
+        os.remove("build/icon.svg")
+        os.remove("build/AppRun")
 
 
 base = "Win32GUI" if sys.platform == "win32" else None
@@ -57,11 +146,14 @@ setup(
             "app/main.py",
             target_name="Nunnix-Manga",
             base=base,
-            icon="app/resources/icons/app.ico"
+            icon=None if sys.platform == "linux"
+            else"app/resources/icons/app.ico"
         )
     ],
     cmdclass={
         "remove_bloat": RemoveBloatFiles,
-        "zip": CreateZipFile
+        "zip": CreateZipFile,
+        "appimage": MakeAppImage,
+        "clean": CleanUp
     },
 )
