3e6ddd394281206250f704ec607d12273ad0d9eb
Nunnito Nevermind <nunnito@pm.me>
2022-06-03 22:51:37 -0600

    Fix: Bad parameter order in data_explorer.py

--- app/core/modules/data_explorer.py
+++ app/core/modules/data_explorer.py
@@ -3,5 +3,18 @@ from ..types import Manga
 
 class DataExplorer(Manga):
     def __init__(self, data: Manga):
-        data_values = locals()["data"].__dict__.values()
-        super(DataExplorer, self).__init__(*data_values)
+        keys = locals()["data"].__dict__
+        data_dict = {
+            "scraper": keys["_scraper"],
+            "title": keys["_title"],
+            "author": keys["_author"],
+            "description": keys["_description"],
+            "cover": keys["_cover"],
+            "genres": keys["_genres"],
+            "link": keys["_link"],
+            "parent": keys["_parent"],
+            "status": keys["_status"],
+            "chapters_data": keys["_chapters_data"],
+        }
+
+        super().__init__(**data_dict)
